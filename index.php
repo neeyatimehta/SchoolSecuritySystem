267e888aead58ad6ba10aaf8ffe98ef72fb64efa
Neeyati <mehtaneeyati@gmail.com>
2021-12-17 08:29:25 +0530

    Setting up Project

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php include "includes/header.php" ?>
+  
+   <div id="wrapper">
+       <!--SIDEBAR-->
+       <?php include "includes/sidebar.php" ?>
+        
+        <div class="d-flex flex-column" id="content-wrapper">
+           
+            <div id="content">
+                <!--NAVBAR-->
+                <?php include "includes/navbar.php" ?>
+                
+                <div class="container-fluid">
+                    <h3 class="text-dark mb-1">Welcome User!</h3>
+                </div>
+                <!-- ./container fluid -->
+            </div>
+            <!-- ./content -->
+            
+<?php include "includes/footer.php"?>
